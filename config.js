a1c451cf06dd0c06bceffbb0e6b4f84506909e08
Eric Cartman <stefan22@users.noreply.github.com>
2016-09-18 17:21:32 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 // exports makes this data available in another file if 
-// I were to require it from that file
+// I were to require from that other file
 // Note: do not add any numbers to your secretKey or 
-// you'll get an error later, while trying to load your 
-// page via localhost like this:
+// you'll get an error later on, while trying to load your 
+// index.html page via localhost like this:
 // "need to authorize this machine using `npm adduser`"
 
 module.exports = {
